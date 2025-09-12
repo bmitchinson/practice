1fec6daed054a364ee57037c882b6e46011cef31
Ben Mitchinson <mitchinson.dev@gmail.com>
2025-09-11 21:01:06 -0700

    layout init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { linkClasses } from '$lib/cssUtils';
 
 	let { children } = $props();
 </script>
@@ -9,4 +10,13 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<header class="mx-8 my-4 flex flex-row items-center justify-between space-x-4 text-2xl">
+	practice
+	<a href="https://mitchinson.dev" class={linkClasses}>my work</a>
+</header>
+
+<hr class="mt-4 mb-4 w-full border-t border-gray-300" />
+
+<div class="flex flex-col items-center justify-between">
+	{@render children?.()}
+</div>
